b49165fc98f81a6bd5bc1b5e60a6d8b290114ee0
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-20 21:06:18 +0530

    commit: socket not connected

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const express = require("express")
-const path = require("path")
-const http = require("http")
-const socketio = require("socket.io")
+/** @format */
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const socketio = require("socket.io");
+let http = require("http");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
 const PORT = process.env.PORT || 3000;
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", socket => {
+  console.log("new connection");
+  // socket.emit("message", "welcome")
+});
 
 app.listen(PORT, () => {
-    console.log(`connected on ${PORT}`)
-})
+  console.log(`connected on ${PORT}`);
+});
